7cf23636c7dd8d4e5399fa41cdc1e2c31ecebcac
carnellj <john.carnell@inin.com>
2016-11-09 09:57:38 -0500

    Latest code

--- authentication-service/src/main/java/com/thoughtmechanix/authentication/Application.java
+++ authentication-service/src/main/java/com/thoughtmechanix/authentication/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,7 +20,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
@@ -58,10 +62,37 @@ public class Application {
         @Autowired
         private UserDetailsService userDetailsService;
 
+
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.tokenStore(tokenStore());                             //JWT
+            endpoints.accessTokenConverter(jwtAccessTokenConverter());      //JWT
             endpoints.authenticationManager(authenticationManager);
             endpoints.userDetailsService(userDetailsService);
+        }
+
+        //JWT
+        @Bean
+        public TokenStore tokenStore() {
+            return new JwtTokenStore(jwtAccessTokenConverter());
+        }
+
+        //JWT
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            converter.setSigningKey("345345fsdfsf5345");
+            return converter;
+        }
+
+        //JWT
+        @Bean
+        @Primary
+        public DefaultTokenServices tokenServices() {
+            DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+            defaultTokenServices.setTokenStore(tokenStore());
+            defaultTokenServices.setSupportRefreshToken(true);
+            return defaultTokenServices;
         }
 
 
