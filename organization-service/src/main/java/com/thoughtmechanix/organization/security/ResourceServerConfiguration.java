51262b96e4ae2630dd273865b9c12608c47b515e
carnellj <john.carnell@inin.com>
2016-11-30 18:01:02 -0500

    Latest code commits.  Working on the OAuth2 resource server

--- organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
@@ -2,6 +2,8 @@ package com.thoughtmechanix.organization.security;
 
 
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -24,6 +26,11 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated();
+        http
+        .authorizeRequests()
+          .antMatchers(HttpMethod.DELETE, "/v1/organizations/**")
+          .hasRole("ADMIN")
+          .anyRequest()
+          .authenticated();
     }
 }
